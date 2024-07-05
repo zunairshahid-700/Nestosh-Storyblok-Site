aaf2a00175c461e1fb7ba9afc05a8b9ba7b0b4a1
Zunair Shahid <Zunair.shahid.c@miracommerce.com>
2024-07-05 16:24:56 +0500

    Homepage other changes

--- src/components/ServicesBlocks.js
+++ src/components/ServicesBlocks.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { storyblokEditable } from '@storyblok/react';
 import './styles/ServicesBlocks.css';
 
+
 const ServicesBlocks = ({ blok }) => {
-     
+    const backgroundImage = blok.ServiceBackgroundImage?.filename || '';
+    const backgroundStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      };
+
+
   return (
-    <div {...storyblokEditable(blok)} className="ServicesBlocks">
-        <h2>{blok.ServiceName}</h2>
-        <img className='ServicesBlockSImages' src={blok.ServiceImage.filename} alt="Logo" />
+    <div {...storyblokEditable(blok)} className="ServicesBlocks column img_bg" style={backgroundStyle} >
+        <p>{blok.ServiceName}</p>
+        <img className='ServicesBlockSImages img_fluid' src={blok.ServiceImage.filename} alt="Logo" />
     </div>
   );
 };
